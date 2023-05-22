596abb8aa5af8bbfc1a51a834ee42aeb4e61b973
Hiroto Nakano <40960166+nakano57@users.noreply.github.com>
2023-05-23 01:15:59 +0900

    Create funnel443.py

---
+++ funnel443.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import subprocess
+import sys
+import json
+
+#print(sys.argv)
+
+if sys.argv[1] == "Get":
+    if sys.argv[3] == "On":
+        result = subprocess.run(["tailscale", "funnel", "status","--json"], stdout=subprocess.PIPE, text=True)
+ #       print(result)
+        result =json.loads(result.stdout)
+        if "AllowFunnel" in result:
+            print(1)
+        else:
+            print(0)
+    sys.exit(0)
+
+if sys.argv[1] == "Set":
+    if sys.argv[3] == "On":
+        if sys.argv[4] == "1":
+            subprocess.run(["sudo", "tailscale", "funnel", "443", "on"])
+            sys.exit(0)
+        else:
+            subprocess.run(["sudo", "tailscale", "funnel", "443", "off"])
+            sys.exit(0)
